719636b22ddd9dd99515aff71993cf499ffc0795
kartik-soni1707 <its.kartiksoni@gmail.com>
2026-01-10 21:21:56 -0800

    CORS oopsies

--- api/contact_me.js
+++ api/contact_me.js
@@ -1,6 +1,9 @@
 import nodemailer from 'nodemailer';
 
 export default async function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method not allowed' });
   }
